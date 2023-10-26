fdc06222935f48a50b01728198feb195ac4323bf
Alexander Garcia <alexg555.ag@gmail.com>
2023-10-25 17:07:07 -0700

    Updated Enemy Script
    Made methods to do healing, damaging, die, and contact damage

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public ushort max_health;
-    public ushort health;
+    public float maxHealth;
+    public float health;
+    public float contactDamage;
     
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,42 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+            die();
+    }
+    
+    void HealEnemy(float heal)
+    {
+        health += heal;
+        if (health > maxHealth)
+            health = maxHealth;
     }
 
+    bool DamageEnemy(float dmg)
+    {
+        //setup to allow a return of boolean before destruction, in case damager has to know if the attack killed the enemy. If multiplayer allows for kill counts per player
+        if (health < dmg)
+        {
+            health = 0;
+            return true;
+        }
+        else
+        {
+            health -= dmg;
+            return false;
+        }
+    }
+
+    public void die()
+    { 
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Damage Player!");
+        //call method on player to do contact damage
+        //collider.gameObject.scriptName.damagePlayer(contactDamage);
+    }
 
 }
