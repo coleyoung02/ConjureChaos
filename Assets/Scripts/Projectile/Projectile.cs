b864b5373196473c05572c57e558cab210e86181
TtroubleTT <TtroubleTT2@gmail.com>
2023-11-01 01:05:22 -0700

    Added camera follow and calling damage for enemies

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
+using Object = System.Object;
 
 public class Projectile : MonoBehaviour
 {
@@ -29,22 +32,13 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D myCollider)
     {
-        Debug.Log(myCollider.name);
-        // This is where we will damage enemy if it is one
-        // Prob keep track of enemies using tags
-        // Prob will make a separate function to override in order to be able to choose damage based on what projectile is
-        DamageEnemy();
+        if (myCollider.CompareTag("Enemy"))
+        {
+            // Is this the best way to do this?
+            Enemy script = myCollider.gameObject.GetComponent<Enemy>();
+            script.DamageEnemy(damage);
+        }
         Destroy(gameObject);
     }
 
-    private void DamageEnemy()
-    {
-        // call Alex's damage enemy method using Damage as parameter
-    }
-    
-    // Im thinking instead of more projectiles we create abilities instead
-    // Most of our powerups planned are either easy stuff like changing speed or damage which seems better to change the damage/speed straight out
-    // Harder stuff like homing could be an ability that when they unlock adds that component ability script to the projectile
-    // Making separate projectiles like we planned seems not the best idea since we arent having different projectiles but we will keep having to stack effects on top of each other instead of them being separate
-    
 }
