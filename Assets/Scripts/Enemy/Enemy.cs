68606762f745c7809a3171f97af5cd87f06b3ede
TtroubleTT <TtroubleTT2@gmail.com>
2023-11-16 19:20:34 -0800

    attempt at status effects

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,10 +14,29 @@ public class Enemy : MonoBehaviour
     public GameObject player;
     PlayerHealth playerHealth;
     ProgressManager progressManager;
+    
+    // Status effect variables
+    private float _statusEffectStart;
+    private ProjectileConjurer _conjurer;
+    // To avoid weird errors with modifying the dictionary by adding and removing. -1 stands for not active effect.
+    private Dictionary<ProjectileConjurer.StatusEffects, float> _effectStartTimes = new();
+    private Dictionary<ProjectileConjurer.StatusEffects, int> _conjurerEffects = new();
 
     // Start is called before the first frame update
     void Start()
     {
+        // Saves the conjurer so we only have to get it once
+        _conjurer = FindObjectOfType<ProjectileConjurer>();
+        
+        // Gets the conjurers Status Effects
+        _conjurerEffects = _conjurer.GetStatusEffects();
+        
+        // Populates Effect Start Times Dict so that we don't have to do it manually when adding new status effects
+        foreach (ProjectileConjurer.StatusEffects effects in Enum.GetValues(typeof(ProjectileConjurer.StatusEffects)))
+        {
+            _effectStartTimes[effects] = -1f;
+        }
+        
         if(!player)
             player = FindAnyObjectByType<PlayerMovement>().gameObject;
         playerHealth = player.GetComponent<PlayerHealth>();
@@ -29,12 +49,14 @@ public class Enemy : MonoBehaviour
             deathEvent.AddListener(progressManager.incrementDeathCounter);
             deathListenerAdded = true;
         }
-            
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Checks to see if the current status effects have expired
+        CheckStatusEffectExpire();
+        
         if (health <= 0)
         {   
             die();
@@ -59,6 +81,9 @@ public class Enemy : MonoBehaviour
         }
         else
         {
+            // Applies status effects if it doesn't have any
+            StatusEffectManager();
+            
             health -= dmg;
             return false;
         }
@@ -82,6 +107,31 @@ public class Enemy : MonoBehaviour
         {
             playerHealth.PlayerTakeDamage(contactDamage);
             die();
+        }
+    }
+    
+    private void StatusEffectManager()
+    {
+        foreach (KeyValuePair<ProjectileConjurer.StatusEffects, int> kvp in _conjurerEffects)
+        {
+            // If we currently dont have this effect
+            if (_effectStartTimes.ContainsKey(kvp.Key) && _effectStartTimes[kvp.Key] == -1)
+            {
+                Debug.Log($"Adding active status effect: {kvp.Key}");
+                // Keeps track of when the status effect was applied
+                _effectStartTimes[kvp.Key] = Time.time;
+            }
+        }
+    }
+    private void CheckStatusEffectExpire()
+    {
+        foreach (KeyValuePair<ProjectileConjurer.StatusEffects, float> kvp in _effectStartTimes)
+        {
+            if (Time.time - kvp.Value >= _conjurerEffects[kvp.Key])
+            {
+                _effectStartTimes[kvp.Key] = -1f;
+                Debug.Log($"Removing active status effect: {kvp.Key}");
+            }
         }
     }
 
