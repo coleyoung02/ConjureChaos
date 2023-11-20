0b62db68523edc5657c520fff9bb2634abfbe345
coleyoung02 <coleyoung741@gmail.com>
2023-11-20 11:24:41 -0800

    build fix

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using Object = System.Object;
 
