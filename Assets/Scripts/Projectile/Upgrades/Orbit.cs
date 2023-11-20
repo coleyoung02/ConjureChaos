b6a1d43931b8e9d207ceb431a3c50cc08c42e688
coleyoung02 <coleyoung741@gmail.com>
2023-11-19 19:24:53 -0800

    shield

--- Assets/Scripts/Projectile/Upgrades/Orbit.cs
+++ Assets/Scripts/Projectile/Upgrades/Orbit.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Orbit : Upgrade
 {
     public override void DoUpgrade()
-    {
-        GameObject g = FindObjectOfType<PlayerMovement>().gameObject;
+    { 
+        FindObjectOfType<ShieldParent>().Activate();
     }
 }
--- Assets/ShieldParent.cs
+++ Assets/ShieldParent.cs
@@ -13,6 +13,11 @@ public class ShieldParent : MonoBehaviour
         transform.Rotate(0f, 0f, Time.deltaTime * rotateSpeed);
     }
 
+    public void Activate()
+    {
+        child.SetActive(true);
+    }
+
     public void OnHit()
     {
         StartCoroutine(disableShiled(downDuration));
