ad59ec259278fe712b7409ebacc977555f2a4ae7
ethanscm <madjuse@uci.edu>
2023-11-30 16:40:22 -0800

    Elements added to pause menu

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -8,6 +8,14 @@ public class PauseManager : MonoBehaviour
     [SerializeField] private GameObject upgradeMenu;
     private bool upgrading;
 
+    private void Start()
+    {
+        if(pauseMenu.activeSelf)
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
