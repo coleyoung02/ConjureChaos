ae614ab7f7ab78bda9404107e49a19c6be576402
coleyoung02 <coleyoung741@gmail.com>
2023-12-02 14:05:18 -0800

    main menu

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
